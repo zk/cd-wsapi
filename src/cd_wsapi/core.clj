2817c15d793676c12600c9ca2ddc125b082b0c45
zkim <zachary.kim@gmail.com>
2010-08-04 01:02:32 -0400

    initial commit

---
+++ src/cd_wsapi/core.clj
@@ -0,0 +1,1 @@
+(ns cd-wsapi.core)
---
+++ test/cd_wsapi/core_test.clj
@@ -0,0 +1,6 @@
+(ns cd-wsapi.core-test
+  (:use [cd-wsapi.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
